a4f5f07f86321008139a4adf137145c25395fad7
lisa fink <lisadfink@gmail.com>
2024-12-14 17:36:09 -0500

    add sounds

--- EmulateSpaceInvaders.c
+++ EmulateSpaceInvaders.c
@@ -6,7 +6,6 @@
 
 #include "8080emulator.h"
 
-
 void ReadFileMem(State8080* state, char* filename, uint32_t mem_address)
 {
     FILE *f= fopen(filename, "rb");
@@ -45,6 +44,8 @@ void InitPorts(Ports* ports)
     ports->output6 = 0;
 }
 
+void PlaySounds(Ports* ports, int port, uint8_t old_bits);
+
 void MachineIN(uint8_t port, Ports* ports, State8080* state)
 {
     switch (port) {
@@ -60,6 +61,8 @@ void MachineIN(uint8_t port, Ports* ports, State8080* state)
         case 3:
             // read shift register with shifted amount
             state->a = ((ports->shift_register >> (8 - ports->shift_amount)) & 0xff);
+            break;
+        default:
             break;
     }
 }
@@ -81,6 +84,8 @@ void MachineOUT(uint8_t port, Ports* ports, State8080* state)
             break;
         case 6:
             ports->output6 = state->a;
+            break;
+        default:
             break;
     }
 }
@@ -249,8 +254,12 @@ int RunCPUCycles(State8080* state, int last_processing, Ports* ports)
         // OUT
         else if (*opcode == 0xd3) {
             uint8_t port = opcode[1];
+            uint8_t old_bits;
+            if (port == 3) old_bits = ports->output3;
+            else if (port == 5) old_bits = ports->output5;
             MachineOUT(port, ports, state);
             state->pc += 2;
+            if (port == 3 || port == 5) PlaySounds(ports, port, old_bits);
         } else
             Emulate8080Op(state);
         cycles -= cycles8080[*opcode];
@@ -263,7 +272,7 @@ int RunCPUCycles(State8080* state, int last_processing, Ports* ports)
 int main(int argc, char**argv)
 {
     // Initialize SDL
-    if (SDL_Init(SDL_INIT_TIMER) != 0) {
+    if (SDL_Init(SDL_INIT_TIMER | SDL_INIT_AUDIO) != 0) {
         printf("SDL_Init Error: %s\n", SDL_GetError());
         return 1;
     }
@@ -332,3 +341,140 @@ int main(int argc, char**argv)
     }
     return 0;
 }
+
+
+void PlaySounds(Ports* ports, int port, uint8_t old_bits)
+{
+    static SDL_AudioSpec wav_spec;
+    static Uint32 wav_length;
+    static Uint8 *wav_buffer;
+    static Uint8 *ufo_buffer = NULL;
+    static int is_playing = 0;
+    static SDL_AudioDeviceID device_id = 0;
+
+    // Init
+    if (device_id == 0) {
+        wav_spec.freq = 44100;
+        wav_spec.format = AUDIO_S16SYS;
+        wav_spec.channels = 1;           // Mono
+        wav_spec.samples = 4096;         // Buffer size
+        wav_spec.callback = NULL;
+
+        device_id = SDL_OpenAudioDevice(NULL, 0, &wav_spec, NULL, 0);
+        if (device_id == 0) {
+            printf("SDL_OpenAudioDevice failed: %s\n", SDL_GetError());
+            SDL_Quit();
+            return;
+        }
+        SDL_LoadWAV("../Rom/Sounds/0.wav", &wav_spec, &ufo_buffer, &wav_length);
+    }
+
+    if (port == 3)
+    {
+        uint8_t bits = ports->output3;
+
+        // UFO sound - 0.wav repeatedly
+        if (bits & 0x1)
+        {
+            printf("Sound: UFO\n");
+            SDL_QueueAudio(device_id, ufo_buffer, wav_length);
+            SDL_PauseAudioDevice(device_id, 0);
+            is_playing = 1;
+        }
+        // Stop UFO sound when the bit is cleared
+        else if (is_playing && ((bits & 0x1) == 0))
+        {
+            SDL_ClearQueuedAudio(device_id);  // Clear the queue
+            is_playing = 0;
+        }
+
+        // Shot sound - 1.wav
+        if ((bits & 0x02) && !(old_bits & 0x02))
+        {
+            if (SDL_LoadWAV("../Rom/Sounds/1.wav", &wav_spec, &wav_buffer, &wav_length) == NULL) {
+                printf("SDL_LoadWAV failed: %s\n", SDL_GetError());
+                return;
+            }
+            SDL_QueueAudio(device_id, wav_buffer, wav_length);
+            SDL_PauseAudioDevice(device_id, 0); // Unpause to start playback
+        }
+
+        // Flash (player die) - 2.wav
+        if ((bits & 0x04) && !(old_bits & 0x04)) {
+            if (SDL_LoadWAV("../Rom/Sounds/2.wav", &wav_spec, &wav_buffer, &wav_length) == NULL) {
+                printf("SDL_LoadWAV failed: %s\n", SDL_GetError());
+                return;
+            }
+            SDL_QueueAudio(device_id, wav_buffer, wav_length);
+            SDL_PauseAudioDevice(device_id, 0); // Unpause to start playback
+        }
+
+        // Invader die - 3.wav
+        if ((bits & 0x08) && !(old_bits & 0x08)) {
+            if (SDL_LoadWAV("../Rom/Sounds/3.wav", &wav_spec, &wav_buffer, &wav_length) == NULL) {
+                printf("SDL_LoadWAV failed: %s\n", SDL_GetError());
+                return;
+            }
+            SDL_QueueAudio(device_id, wav_buffer, wav_length);
+            SDL_PauseAudioDevice(device_id, 0); // Unpause to start playback
+        }
+    }
+    // Port 5
+    else
+    {
+        uint8_t bits = ports->output5;
+
+        // Fleet movement 1 - 4.wav
+        if ((bits & 0x02) && !(old_bits & 0x02))
+        {
+            if (SDL_LoadWAV("../Rom/Sounds/4.wav", &wav_spec, &wav_buffer, &wav_length) == NULL) {
+                printf("SDL_LoadWAV failed: %s\n", SDL_GetError());
+                return;
+            }
+            SDL_QueueAudio(device_id, wav_buffer, wav_length);
+            SDL_PauseAudioDevice(device_id, 0); // Unpause to start playback
+        }
+
+        // Fleet movement 2 - 5.wav
+        if ((bits & 0x02) && !(old_bits & 0x02))
+        {
+            if (SDL_LoadWAV("../Rom/Sounds/5.wav", &wav_spec, &wav_buffer, &wav_length) == NULL) {
+                printf("SDL_LoadWAV failed: %s\n", SDL_GetError());
+                return;
+            }
+            SDL_QueueAudio(device_id, wav_buffer, wav_length);
+            SDL_PauseAudioDevice(device_id, 0); // Unpause to start playback
+        }
+
+        // Fleet movement 3 - 6.wav
+        if ((bits & 0x04) && !(old_bits & 0x04)) {
+            if (SDL_LoadWAV("../Rom/Sounds/6.wav", &wav_spec, &wav_buffer, &wav_length) == NULL) {
+                printf("SDL_LoadWAV failed: %s\n", SDL_GetError());
+                return;
+            }
+            SDL_QueueAudio(device_id, wav_buffer, wav_length);
+            SDL_PauseAudioDevice(device_id, 0); // Unpause to start playback
+        }
+
+        // Fleet movement 4 - 7.wav
+        if ((bits & 0x08) && !(old_bits & 0x08)) {
+            if (SDL_LoadWAV("../Rom/Sounds/7.wav", &wav_spec, &wav_buffer, &wav_length) == NULL) {
+                printf("SDL_LoadWAV failed: %s\n", SDL_GetError());
+                return;
+            }
+            SDL_QueueAudio(device_id, wav_buffer, wav_length);
+            SDL_PauseAudioDevice(device_id, 0); // Unpause to start playback
+        }
+
+        // UFO Hit - 8.wav
+        if ((bits & 0x10) && !(old_bits & 0x10)) {
+            if (SDL_LoadWAV("../Rom/Sounds/8.wav", &wav_spec, &wav_buffer, &wav_length) == NULL) {
+                printf("SDL_LoadWAV failed: %s\n", SDL_GetError());
+                return;
+            }
+            SDL_QueueAudio(device_id, wav_buffer, wav_length);
+            SDL_PauseAudioDevice(device_id, 0); // Unpause to start playback
+        }
+    }
+}
+
