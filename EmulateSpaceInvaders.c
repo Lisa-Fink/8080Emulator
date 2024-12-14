d51bca2d98db3aaa696106db15f04abd85c407c6
lisa fink <lisadfink@gmail.com>
2024-12-13 19:09:02 -0500

    add ports

--- EmulateSpaceInvaders.c
+++ EmulateSpaceInvaders.c
@@ -20,11 +20,77 @@ void ReadFileMem(State8080* state, char* filename, uint32_t mem_address)
     fclose(f);
 }
 
+typedef struct Ports {
+    // input-read, output-write
+    uint8_t input0, input1, input2, output2, output3, output5, output6;
+    uint16_t shift_register;
+    uint8_t shift_amount;
+} Ports;
+
+void InitPorts(Ports* ports)
+{
+    ports->input0 = 0b00001110; // Bits 1, 2, 3 are always 1; other inputs are default 0.
+    ports->input1 = 0b00001000; // Bit 3 is always 1; all others default to 0.
+    ports->input2 = 0b00000000;
+
+    ports->shift_register = 0x0000;
+    ports->shift_amount = 0;
+    ports->output2 = 0;
+    ports->output3 = 0;
+    ports->output5 = 0;
+    ports->output6 = 0;
+}
+
+void MachineIN(uint8_t port, Ports* ports, State8080* state)
+{
+    switch (port) {
+        case 0:
+            state->a = ports->input0;
+            break;
+        case 1:
+            state->a = ports->input1;
+            break;
+        case 2:
+            state->a = ports->input2;
+            break;
+        case 3:
+            // read shift register with shifted amount
+            state->a = ports->shift_register >> (8 - ports->shift_amount);
+            break;
+    }
+}
+void MachineOUT(uint8_t port, Ports* ports, State8080* state)
+{
+    switch (port) {
+        case 2:
+            ports->shift_amount = state->a & 0x7;
+            break;
+        case 3:
+            ports->output3 = state->a;
+            break;
+        case 4:
+            // shift register moves left half to right side, and place new value on left side
+            ports->shift_register = (state->a << 8) | (ports->shift_register >> 8);
+            break;
+        case 5:
+            ports->output5 = state->a;
+            break;
+        case 6:
+            ports->output6 = state->a;
+            break;
+    }
+}
+
+
+
 int main (int argc, char**argv)
 {
     // Initialize states
     State8080* state = calloc(1, sizeof(State8080));
     state->memory = malloc(0x10000);  // 16K
+
+    Ports* ports = calloc(1, sizeof(Ports));
+    InitPorts(ports);
 
     // Read files into state[memory]
     ReadFileMem(state, "../Rom/invaders.h", 0);
@@ -36,7 +102,28 @@ int main (int argc, char**argv)
 //    while (state->pc < 0x2000)
     while (line < 50000)
     {
-        Emulate8080Op(state);
+        unsigned char *opcode = &state->memory[state->pc];
+
+        // Game has specific function for IN/OUT, which isn't in the general emulator function
+        // IN
+        if (*opcode == 0xdb)
+        {
+            uint8_t port = opcode[1];
+            MachineIN(port, ports, state);
+            state->pc += 2;
+        }
+        // OUT
+        else if (*opcode == 0xd3)
+        {
+            uint8_t port = opcode[1];
+            MachineOUT(port, ports, state);
+            state->pc += 2;
+        }
+        else
+            Emulate8080Op(state);
+
+
+
         // Print for debugging
         printf("\t");
         printf("%c", state->cc.z ? 'z' : '.');
